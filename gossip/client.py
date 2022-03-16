20259a5bd8f7d0a13ff9701925692e65e7902146
Jethro Cao <jethrocao@gmail.com>
2022-03-15 21:24:15 -0400

    Create host_port_tup as property of GossipClient & use

--- gossip/client.py
+++ gossip/client.py
@@ -5,21 +5,21 @@ class GossipClient:
     """A client interface to connect to a server in a peer-to-peer gossip network."""
 
     def __init__(self, address):
-        self.host, port = address.split(":")
-        self.port = int(port)
+        host, port = address.split(":")
+        self.host_port_tup = (host, int(port))
 
     def send_message(self, message, is_relay=False):
         """Send a message to the server."""
 
-        cmd, msg = ("/RELAY", json.dumps(message)) if is_relay else ("/NEW", message)
+        cmd = "/RELAY" if is_relay else "/NEW"
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((self.host, self.port))
-            sock.send(bytes(f"{cmd}:{msg}", "utf-8"))
+            sock.connect(self.host_port_tup)
+            sock.send(bytes(f"{cmd}:{message}", "utf-8"))
 
     def get_messages(self):
         """Fetch a list of all messages stored by the server."""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((self.host, self.port))
+            sock.connect(self.host_port_tup)
             sock.send(bytes("/GET:\n", "utf-8"))
 
             recvd_msgs_ls = []
